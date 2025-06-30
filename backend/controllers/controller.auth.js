30161eb7d08e8f0e1f815797cb1e78f94289d0dd
Deepak <vashistdeepak138@gmail.com>
2025-06-30 11:53:17 +0530

    Added register in backend

--- backend/controllers/controller.auth.js
+++ backend/controllers/controller.auth.js
@@ -12,8 +12,22 @@ const register = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new User({name, email, password: hashedPassword});
         await newUser.save();
-        
-        res.status(201).json({message: 'User registered succewssfully'});
+        const token = generateToken(user._id);
+        const options = {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'Lax',
+            maxAge: 3600000 // 1 hour
+        };
+        return res
+            .cookie("BearerToken", token, options)
+            .status(200)
+            .json({
+                token,
+                user: { id: user._id, name: user.name, email: user.email },
+                message: 'Register successful. Taking you to Quiz page'
+            });
+        // res.status(201).json({message: 'User registered successfully'});
     } catch (error) {
         res.status(500).json({message: 'Error registering user', error: error.message});
     }
