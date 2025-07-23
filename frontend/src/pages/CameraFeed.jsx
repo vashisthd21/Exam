bb79d74f83334c9eac77993550bf4d6de5945520
Deepak <vashistdeepak138@gmail.com>
2025-07-23 23:18:30 +0530

    Updates in Quiz

--- frontend/src/pages/CameraFeed.jsx
+++ frontend/src/pages/CameraFeed.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from 'react';
-import * as faceapi from 'face-api.js';
 
 const styles = {
   cameraContainer: {
@@ -20,18 +19,10 @@ const styles = {
     height: '100%',
     objectFit: 'cover',
   },
-  canvas: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-  },
 };
 
-export default function CameraFeed({ onMultipleFacesDetected, onNoFaceDetected }) {
+export default function CameraFeed() {
   const videoRef = useRef(null);
-  const canvasRef = useRef(null);
 
   useEffect(() => {
     const startVideo = async () => {
@@ -45,12 +36,7 @@ export default function CameraFeed({ onMultipleFacesDetected, onNoFaceDetected }
       }
     };
 
-    const loadModels = async () => {
-      const MODEL_URL = '/models'; // or CDN path for models
-      await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
-    };
-
-    loadModels().then(startVideo);
+    startVideo();
 
     return () => {
       // Cleanup video stream on unmount
@@ -59,38 +45,6 @@ export default function CameraFeed({ onMultipleFacesDetected, onNoFaceDetected }
       }
     };
   }, []);
-
-  useEffect(() => {
-    let intervalId;
-
-    const detectFaces = async () => {
-      if (!videoRef.current || videoRef.current.readyState !== 4) return;
-
-      const options = new faceapi.TinyFaceDetectorOptions();
-
-      const detections = await faceapi.detectAllFaces(videoRef.current, options);
-
-      const canvas = canvasRef.current;
-      const displaySize = { width: videoRef.current.videoWidth, height: videoRef.current.videoHeight };
-
-      faceapi.matchDimensions(canvas, displaySize);
-      const resizedDetections = faceapi.resizeResults(detections, displaySize);
-
-      const ctx = canvas.getContext('2d');
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      faceapi.draw.drawDetections(canvas, resizedDetections);
-
-      if (detections.length === 0) {
-        onNoFaceDetected && onNoFaceDetected();
-      } else if (detections.length > 1) {
-        onMultipleFacesDetected && onMultipleFacesDetected();
-      }
-    };
-
-    intervalId = setInterval(detectFaces, 1000);
-
-    return () => clearInterval(intervalId);
-  }, [onMultipleFacesDetected, onNoFaceDetected]);
 
   return (
     <div style={styles.cameraContainer}>
@@ -102,12 +56,6 @@ export default function CameraFeed({ onMultipleFacesDetected, onNoFaceDetected }
         style={styles.video}
         width={180}
         height={135}
-      />
-      <canvas
-        ref={canvasRef}
-        width={180}
-        height={135}
-        style={styles.canvas}
       />
     </div>
   );
