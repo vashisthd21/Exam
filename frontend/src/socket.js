4efe44107876ec5fb8626efb3cb59b087fd9b24f
Radhika Jindal <148737056+radhikajindal13@users.noreply.github.com>
2026-03-03 13:53:28 +0530

    Update socket connection URL to production server

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,4 +1,5 @@
 import io from 'socket.kio-client';
-const socket = io('http://loclalhost:5000');
+// const socket = io('http://loclalhost:5000');
+const socket = io('https://exam-86ot.onrender.com');
 
 export default socket;
